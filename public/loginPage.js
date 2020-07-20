dcdba3120b46d7e1ee085bdbf3e01a356cea60f9
pejma2001 <pejma2001@mail.ru>
2020-07-20 23:38:46 +0400

    new commit

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,13 +1,23 @@
 "use strict";
 const userForm = new UserForm();
-try{
-userForm.loginFormCallback = function log (data){
-    ApiConnector.login({login, password}, callback);
-    loginFormCallback();
-    location.reload();
 
+userForm.loginFormCallback = ({login, password}) =>
+    ApiConnector.login({login, password}, userForm.loginFormCallback());
+
+
+
+
+///Пример по деструктуризации:
+let city = {
+    population: 1000000,
+    age: 187,
+    size: {
+    width: 7400,
+    length: 6300
+    }
+};
+function aboutCity({ population, age, size:{width, length}, name = "Омск" }) {
+console.log(`В городе ${name} с количеством населения ${population} и возрастом ${age} лет размер центрального парка составляет ${width} x ${length} метров`);
 }
-}
-catch (err){
-    alert(err);
-}
+
+aboutCity(city);
